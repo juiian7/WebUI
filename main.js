1bc9a9e616363fc0c5cb1a03bd15b208b609c446
ZupaJul <44403676+juiian7@users.noreply.github.com>
2022-04-20 10:06:09 +0200

    Add client side router and link component (#2)

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
-import { button, div, h1, h2, span, p, dynamic, input, form, list } from "./dist/index.js";
+import { button, div, h1, h2, span, p, dynamic, input, form, list, router, useRouter, update, link } from "./dist/index.js";
 
 let counter = 0;
-
-let todos = [];
-let todo = "";
-
-function addTodo() {
-    todos = [...todos, todo];
-    todo = "";
-}
-let todoComponent = div(
-    h1("Todos"),
-    dynamic(
-        () =>
-            form(
-                input(todo)
-                    .on("change", (e) => (todo = e.target.value))
-                    .focus(),
-                button("Add todo").type("submit")
-            )
-                .noDefault()
-                .on("submit", addTodo),
-        () => [todos]
-    ),
-    dynamic(
-        () => list(todos.map((todo) => p(todo))),
-        () => [todos]
-    )
-);
-
-let rootDiv = div(
+let mainPage = div(
     h1("Hoi!"),
     h2("Hoi2!"),
 
@@ -52,7 +24,67 @@ let rootDiv = div(
         .color("#44DD44"),
 
     button("Reset").on("click", () => window.confirm("You sure?") && (counter = 0)),
-    todoComponent
+
+    div(button("Todo's").on("click", () => router().redirect("/todo"))),
+
+    div(link('Own link: "/todo?items=apple,banana"', "/todo?items=apple,banana")),
+    div(link("GitHub link", "https://github.com/juiian7/WebUI"))
 );
 
-document.body.append(rootDiv.HTML);
+let todos = [];
+let todo = "";
+function addTodo() {
+    todos = [...todos, todo];
+    todo = "";
+}
+let todoPage = div(
+    link("<- Home", "/"),
+    h1("Todos"),
+    dynamic(
+        () =>
+            form(
+                input(todo)
+                    .on("change", (e) => (todo = e.target.value))
+                    .focus(),
+                button("Add todo").type("submit")
+            )
+                .noDefault()
+                .on("submit", addTodo),
+        () => [todos]
+    ),
+    dynamic(
+        () => list(todos.map((todo) => div(link(todo, "/todo/" + todo + "/info?param1=test&hello=world")))),
+        () => [todos]
+    )
+);
+
+// setup router
+useRouter().on("/", mainPage);
+
+// access router
+router()
+    .on("/todo", (params) => {
+        if (params["items"]) {
+            params["items"].split(",").forEach((item) => {
+                todo = item;
+                addTodo();
+            });
+        }
+
+        console.log(todos);
+        update();
+        return todoPage;
+    })
+    .on("/todo/:item/info", (params, vars) => {
+        let e = div(h1(vars["item"]));
+
+        for (const key in params) {
+            e.append(p(key + ": " + params[key]));
+        }
+
+        e.append(link("<- back", "/todo/"));
+
+        return e;
+    })
+    // redirect to active page
+    .sync();
--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,12 @@ export { button } from "./Components/button.js";
 export { input } from "./Components/input.js";
 
 export { img } from "./Components/img.js";
+export { link } from "./Components/link.js";
 
 export { list } from "./Components/list.js";
 
 export { element } from "./Components/element.js";
 
 export { dynamic, update } from "./dynamic.js";
+
+export { router, useRouter } from "./router.js";
