f54666bf62ae4b2e082c6356057b88731ce57fa6
ZupaJul <44403676+juiian7@users.noreply.github.com>
2022-07-24 22:07:33 +0200

    Fix div layouts (#3)

--- src/Components/div.ts
+++ src/Components/div.ts
@@ -1,11 +1,31 @@
 import Base, { Elem } from "../base.js";
 
+const vboxCSS = ".vbox>*{display:block;}";
+const hboxCSS = ".hbox{display:flex;flex:direction:row;align-items:center;justify-content:space-evenly;}";
+
+const style = document.createElement("style");
+style.innerHTML = vboxCSS + hboxCSS;
+document.getElementsByTagName("head")[0].append(style);
+
 export function div(...params: Elem[]) {
     return new Div(...params);
+}
+
+export function hbox(...children: Elem[]) {
+    return div(...children).class("hbox");
+}
+
+export function vbox(...children: Elem[]) {
+    return div(...children).class("vbox");
 }
 
 class Div extends Base<HTMLDivElement> {
     constructor(...params: Elem[]) {
         super("div", ...params);
+    }
+
+    public width(w: string) {
+        this._style.width = w;
+        return this;
     }
 }
