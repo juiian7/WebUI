925489c0d39dc6e12e2f77a56fe80414c36d6122
ZupaJul <44403676+juiian7@users.noreply.github.com>
2022-09-08 11:40:40 +0200

    Remove list component

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,6 @@ export { input } from "./Components/input.js";
 export { img } from "./Components/img.js";
 export { link } from "./Components/link.js";
 
-export { list } from "./Components/list.js";
-
 export { element } from "./Components/element.js";
 
 export { dynamic, update } from "./dynamic.js";
